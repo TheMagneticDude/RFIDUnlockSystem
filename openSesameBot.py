464b43c1bd9c3ff0b8cdbec23d2c445c7df8f621
TheMagneticDude <110270128+TheMagneticDude@users.noreply.github.com>
2026-04-05 19:59:14 -0400

    Update openSesameBot.py

--- openSesameBot.py
+++ openSesameBot.py
@@ -39,7 +39,7 @@ class Client(commands.Bot):
 
 #discord UI component
 class View(discord.ui.View):
-    @discord.ui.button(label="Click", style=discord.ButtonStyle.primary, emoji="🧲")
+    @discord.ui.button(label="Unlock", style=discord.ButtonStyle.primary, emoji="🧲")
     async def button_callback(self, button, interaction):
         await button.response.send_message("Button detected!");
  
@@ -66,10 +66,9 @@ async def doorCommand(interaction: discord.Interaction):
     embed = discord.Embed(title = "Door State", description = "TestState", color = discord.Color.green());
     embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/805648700365209631/1419696609620398213/Magnets_symbol128x128.png?ex=69d3c780&is=69d27600&hm=e2d3edfc0e19da48f838cb84a7f7c570af1714d7c5ab316d3ed9cb4c9f89f798&");
     await interaction.response.send_message(embed = embed);
-
-@client.tree.command(name="button", description="button", guild = GUILD_ID)
-async def button(interaction: discord.Interaction):    
     await interaction.response.send_message(view=View());
+
+
 
 
 
