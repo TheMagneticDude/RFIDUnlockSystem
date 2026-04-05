d39ef51b4b18642b35665d14a4ca831ab77db8bc
TheMagneticDude <110270128+TheMagneticDude@users.noreply.github.com>
2026-04-05 19:26:33 -0400

    actually secure bot token thing

--- openSesameBot.py
+++ openSesameBot.py
@@ -1,29 +1,48 @@
 import discord
+from discord.ext import commands
+from discord import app_commands
+import os
 
-class Client(discord.Client):
+#========================= Constants =========================
+#will fill in after to avoid secret being in repo
+botToken = '';
+#grab token from local env variable
+botToken = os.getenv("DISCORD_TOKEN");
+
+GUILD_ID = discord.Object(id=1490450047085838446);
+
+
+#========================= Class =========================
+class Client(commands.Bot):
     async def on_ready(self):
         print(f'Logged on as {self.user}!');
         
     async def on_message(self, message):
         if message.author == self.user:
             return
-        if message.content.startswith('sesame'):
-            await message.channel.send(f'Command Recieved: {message.author} executed {message.content}');
+        if message.content.startswith('!sesame'):
+            await message.channel.send(f'Command Recieved: {message.author} executed: {message.content}');
 
-    
+    @client.tree.command(name="door", description="Controls Door", guild = GUILD_ID);
+    async def printer(interaction: discord.Interaction):
+        embed = discord.Embed(title = "Door State", description = "TestState");
+        await interaction.response.send_message(embed = embed);
  
     
     
     
-    
-    
+#========================= Main stuffs =========================
+
+
 intents = discord.Intents.default();
 intents.message_content = True;
+client = Client(command_prefix = "!sesame", intents = intents);
 
-#will fill in after to avoid secret being in repo
-botToken = '';
+
+
+
+
 
 
 #run bot
-client = Client(intents = intents);
 client.run(botToken);
