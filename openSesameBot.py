bdfaaf2d8d987f9e3b83b5fc5b44f8474b1dc79c
TheMagneticDude <110270128+TheMagneticDude@users.noreply.github.com>
2026-04-05 18:59:38 -0400

    enable intents

--- openSesameBot.py
+++ openSesameBot.py
@@ -5,7 +5,7 @@ class Client(discord.Client):
         print(f'Logged on as {self.user}!');
         
 intents = discord.Intents.default();
-#intents.message_content = True;
+intents.message_content = True;
 
 #will fill in after to avoid secret being in repo
 botToken = '';
