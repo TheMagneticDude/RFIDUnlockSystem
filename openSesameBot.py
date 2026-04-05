1914d28bf1a977450745b313254b9e2b4ca6d03e
TheMagneticDude <110270128+TheMagneticDude@users.noreply.github.com>
2026-04-05 17:17:51 -0400

    fix intent issue

--- openSesameBot.py
+++ openSesameBot.py
@@ -1,16 +1,16 @@
 import discord
 
 class Client(discord.Client):
-    async def on_read(self):
+    async def on_ready(self):
         print(f'Logged on as {self.user}!');
         
 intents = discord.Intents.default();
-intents.message_content = True;
+#intents.message_content = True;
 
+#will fill in after to avoid secret being in repo
+botToken = '';
 
 
 #run bot
 client = Client(intents = intents);
-
-#will fill in after to avoid secret being in repo
-client.run('');
+client.run(botToken);
