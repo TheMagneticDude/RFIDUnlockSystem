20c994632151323323ed7b4007980f62b29d39a1
TheMagneticDude <110270128+TheMagneticDude@users.noreply.github.com>
2026-04-05 19:39:18 -0400

    Update openSesameBot.py

--- openSesameBot.py
+++ openSesameBot.py
@@ -20,12 +20,22 @@ GUILD_ID = discord.Object(id=1490450047085838446);
 class Client(commands.Bot):
     async def on_ready(self):
         print(f'Logged on as {self.user}!');
+        try:
+            synced = await self.tree.sync(guild=GUILD_ID)
+            print(f"Synced {len(synced)} command(s) to guild.")
+        except Exception as e:
+            print(e)
         
     async def on_message(self, message):
-        if message.author == self.user:
-            return
-        if message.content.startswith('!sesame'):
-            await message.channel.send(f'Command Recieved: {message.author} executed: {message.content}');
+    if message.author == self.user:
+        return
+
+    if message.content.startswith('!sesame'):
+        await message.channel.send(
+            f'Command Recieved: {message.author} executed: {message.content}'
+        )
+
+    await self.process_commands(message)
 
     
  
