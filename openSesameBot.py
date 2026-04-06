c299533bf5c1f6bf5863cb06e41a10068b3b09bf
TheMagneticDude <110270128+TheMagneticDude@users.noreply.github.com>
2026-04-05 20:40:32 -0400

    Update openSesameBot.py

--- openSesameBot.py
+++ openSesameBot.py
@@ -11,7 +11,38 @@ botToken = os.getenv("DISCORD_TOKEN");
 
 GUILD_ID = discord.Object(id=1490450047085838446);
 
+#========================= Global States =========================
+embed_door_open = False
+door_message = None # discord message
+embed_unlocked = False;
 
+#door state to track if door is open or not
+    #closed by default
+doorState = false;
+#door unlocked hardware state
+doorUnlockedState = False;
+
+
+embedColor = discord.Color.red();
+embedTitle = "Door State: Locked 🔒"
+#discord.Color.green();
+#"Door State: Unlocked 🔓"
+
+
+def build_door_embed():
+    global embed_unlocked, embed_door_open
+    
+    lock_state = "Unlocked 🔓" if embed_unlocked else "Locked 🔒"
+    door_state = "Open 🚪" if embed_door_open else "Closed 🚪"
+    
+    color = discord.Color.green() if embed_unlocked else discord.Color.red()
+    
+    embed = discord.Embed(
+        title=f"Door is {lock_state}",
+        description=f"Physical Door State: **{door_state}**",
+        color=color
+    )
+    return embed
 
  
 
@@ -36,7 +67,28 @@ class Client(commands.Bot):
             )
 
         await self.process_commands(message)
-
+        
+        
+        
+        
+    #async background thread to update door message
+    @tasks.loop(seconds=5.0) # updates every 5 seconds
+    async def hardware_monitor(self):
+        global door_message, embed_door_open, embed_unlocked
+        if door_message is None:
+                    return # door command has not been run yet
+        if doorState != embed_door_open or doorUnlockedState != embed_unlocked:
+            embed_door_open = doorState;
+            embed_unlocked = doorUnlockedState;
+            #update variables if any of them are out of sync
+            
+            try:
+                # Edit the saved message directly using the Discord API
+                await door_message.edit(embed=build_door_embed(), view=ViewButton())
+                print("Discord message dynamically updated from hardware state!")
+            except discord.NotFound:
+                # The message was deleted in Discord
+                door_message = None
 
 
  
@@ -44,32 +96,18 @@ class Client(commands.Bot):
 
 
 
-unlocked = False;
-
-embedColor = discord.Color.red();
-embedTitle = "Door State: Locked 🔒"
-#discord.Color.green();
-#"Door State: Unlocked 🔓"
-
-
-def build_door_embed():
-    global unlocked
-    if unlocked:
-        return discord.Embed(title="Door State: Unlocked 🔓",description="Door is currently unlocked",color=discord.Color.green())
-    else:
-        return discord.Embed(title="Door State: Locked 🔒",description="Door is currently locked",color=discord.Color.red())
-        
         
 
-#discord UI button component
+#========================= UI Components =========================
 class ViewButton(discord.ui.View):
     
     @discord.ui.button(label="Unlock", style=discord.ButtonStyle.primary, emoji="🧲")
     async def button_callback(self, interaction: discord.Interaction, button: discord.ui.Button):
-        global unlocked
-        #update embed on button press
-        unlocked = not unlocked;
-       
+        global embed_unlocked
+        #send door command on button press
+        
+        #wait 1 s for response
+        time.sleep(1);
         #update message
         await interaction.response.edit_message(embed=build_door_embed(), view=self);
  
@@ -93,7 +131,9 @@ async def test(interaction: discord.Interaction):
     
 @client.tree.command(name="door", description="Controls Door", guild = GUILD_ID)
 async def doorCommand(interaction: discord.Interaction):
+    global door_message
     await interaction.response.send_message(embed = build_door_embed(), view=ViewButton());
+    door_message = await interaction.original_response()
 
 
 
